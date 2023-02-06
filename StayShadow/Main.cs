9cf20e8a11f94ce425e0019c951e83b4a1f40386
InvertedOwl <70726294+InvertedOwl@users.noreply.github.com>
2023-02-05 17:37:06 -0700

    Add project files.

---
+++ StayShadow/Main.cs
@@ -0,0 +1,45 @@
+﻿using MelonLoader;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace StayShadow
+{
+    public static class BuildInfo
+    {
+        public const string Name = "StayShadow"; // Name of the Mod.  (MUST BE SET)
+        public const string Description = "The choice between shadow or not persists"; // Description for the Mod.  (Set as null if none)
+        public const string Author = "InvertedOwl"; // Author of the Mod.  (MUST BE SET)
+        public const string Company = null; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "0.0.1"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    public class StayShadow : MelonMod
+    {
+
+        public override void OnSceneWasLoaded(int buildindex, string sceneName) 
+        {
+            GameObject shadowToggle = GameObject.Find("Toggle Shadows");
+
+            MelonLogger.Msg(PlayerPrefs.GetString("shadow"));
+
+            if (PlayerPrefs.HasKey("shadow"))
+            {
+                if (PlayerPrefs.GetString("shadow") == "False")
+                {
+                    shadowToggle.GetComponent<Toggle>().isOn = !shadowToggle.GetComponent<Toggle>().isOn;
+                    GameObject.Find("Directional Light").GetComponent<Light>().shadows = LightShadows.None;
+                }
+            }
+            shadowToggle.GetComponent<Toggle>().onValueChanged.AddListener(delegate
+            {
+                OnShadowToggle(shadowToggle.GetComponent<Toggle>());
+            });
+        }
+
+        public void OnShadowToggle(Toggle t)
+        {
+            PlayerPrefs.SetString("shadow", (t.isOn) + "");
+        }
+    }
+}
---
+++ StayShadow/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using MelonLoader;
+using System.Reflection;
+
+[assembly: AssemblyTitle(StayShadow.BuildInfo.Description)]
+[assembly: AssemblyDescription(StayShadow.BuildInfo.Description)]
+[assembly: AssemblyCompany(StayShadow.BuildInfo.Company)]
+[assembly: AssemblyProduct(StayShadow.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + StayShadow.BuildInfo.Author)]
+[assembly: AssemblyTrademark(StayShadow.BuildInfo.Company)]
+[assembly: AssemblyVersion(StayShadow.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(StayShadow.BuildInfo.Version)]
+[assembly: MelonInfo(typeof(StayShadow.StayShadow), StayShadow.BuildInfo.Name, StayShadow.BuildInfo.Version, StayShadow.BuildInfo.Author, StayShadow.BuildInfo.DownloadLink)]
+[assembly: MelonColor()]
+
+// Create and Setup a MelonGame Attribute to mark a Melon as Universal or Compatible with specific Games.
+// If no MelonGame Attribute is found or any of the Values for any MelonGame Attribute on the Melon is null or empty it will be assumed the Melon is Universal.
+// Values for MelonGame Attribute can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame(null, null)]
